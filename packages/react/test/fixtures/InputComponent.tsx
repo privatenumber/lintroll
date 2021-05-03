1d95d4f62309b1f54ad1b165c3796d2c6a6bb43f
Hiroki Osame <hiroki.osame@gmail.com>
2021-05-03 02:44:25 -0400

    feat: added react package

---
+++ packages/react/test/fixtures/InputComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const InputComponent = () => (
+	<input
+		title="value"
+		disabled
+	/>
+);
+
+export default InputComponent;
---
+++ packages/react/test/fixtures/fail.tsx
@@ -0,0 +1,10 @@
+
+export const InputComponent = ({ value }) => (
+	<input value={value} title='some-title' disabled />
+);
+
+export const Button = ({ value }) => (
+  <button value={value}>
+    Label
+  </button>
+);
