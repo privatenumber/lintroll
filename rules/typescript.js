a88ff5183573e8f0b5ea7657908732e42a58693b
Hiroki Osame <hiroki.osame@gmail.com>
2021-10-19 14:08:20 -0400

    fix: detect typescript

--- rules/typescript.js
+++ rules/typescript.js
@@ -10,77 +10,93 @@ const noExtraneousDependenciesConfig = baseImports.rules['import/no-extraneous-d
 
 /** @typedef { import('eslint').Linter.Config } ESLintConfig */
 
-/** @type { ESLintConfig } */
-const config = {
-	overrides: [
-		// Setting as an override allows .ts files to be
-		// linted without specifying it on the user-end
-		{
-			files: '*.{ts,tsx}',
-
-			extends: [
-				'plugin:@typescript-eslint/recommended',
-
-				// https://github.com/benmosher/eslint-plugin-import/blob/6c8981d/config/typescript.js
-				'plugin:import/typescript',
-			],
-
-			settings: {
-				'import/resolver': {
-					typescript: {},
-				},
-			},
-
-			parserOptions: {
-				// Gets closest tsconfig.json
-				project: '**/tsconfig.json',
-			},
-
-			rules: {
-				// Always require await when returning promise
-				// https://github.com/goldbergyoni/nodebestpractices/blob/5ba537d/sections/errorhandling/returningpromises.md
-				'@typescript-eslint/return-await': ['error', 'always'],
-
-				'import/no-extraneous-dependencies': ['error', {
-					...noExtraneousDependenciesConfig,
-					devDependencies: [
-						...noExtraneousDependenciesConfig.devDependencies.map(
-							pattern => pattern.replace('.js', '.{js,ts}'),
-						),
-					],
-				}],
-
-				// TS disallows .ts extension
-				// https://github.com/Microsoft/TypeScript/issues/27481
-				'import/extensions': ['error', 'ignorePackages', {
-					ts: 'never',
-					tsx: 'never',
-				}],
-
-				'node/file-extension-in-import': ['error', 'always', {
-					'.ts': 'never',
-					'.tsx': 'never',
-				}],
-
-				'@typescript-eslint/member-delimiter-style': 'error',
-
-				'@typescript-eslint/consistent-type-assertions': 'error',
-
-				// Allow functions to be passed in only in TS because it's easy to see their types
-				'unicorn/no-array-callback-reference': 'off',
-
-				// Not always possible to destructue at top-level when the variable is ambigious
-				'unicorn/consistent-destructuring': 'off',
-
-				// Could be used to pass in an explicit `undefined` to a required parameter
-				'unicorn/no-useless-undefined': 'off',
-
-				// https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-shadow.md
-				'no-shadow': 'off',
-				'@typescript-eslint/no-shadow': 'error',
-			},
-		},
-	],
+const hasTypeScript = () => {
+	try {
+		// eslint-disable-next-line node/no-unpublished-require
+		require.resolve('typescript');
+		return true;
+	} catch {
+		return false;
+	}
 };
+
+/** @type { ESLintConfig } */
+const config = (
+	hasTypeScript()
+		? {
+			overrides: [
+				// Setting as an override allows .ts files to be
+				// linted without specifying it on the user-end
+				{
+					files: '*.{ts,tsx}',
+
+					extends: [
+						'plugin:@typescript-eslint/recommended',
+
+						// https://github.com/benmosher/eslint-plugin-import/blob/6c8981d/config/typescript.js
+						'plugin:import/typescript',
+					],
+
+					settings: {
+						'import/resolver': {
+							typescript: {},
+						},
+					},
+
+					parserOptions: {
+						// Gets closest tsconfig.json
+						project: '**/tsconfig.json',
+					},
+
+					rules: {
+						// Always require await when returning promise
+						// https://github.com/goldbergyoni/nodebestpractices/blob/5ba537d/sections/errorhandling/returningpromises.md
+						'@typescript-eslint/return-await': ['error', 'always'],
+
+						'import/no-extraneous-dependencies': ['error', {
+							...noExtraneousDependenciesConfig,
+							devDependencies: [
+								...noExtraneousDependenciesConfig.devDependencies.map(
+									pattern => pattern.replace('.js', '.{js,ts}'),
+								),
+							],
+						}],
+
+						// TS disallows .ts extension
+						// https://github.com/Microsoft/TypeScript/issues/27481
+						'import/extensions': ['error', 'ignorePackages', {
+							ts: 'never',
+							tsx: 'never',
+						}],
+
+						'node/file-extension-in-import': ['error', 'always', {
+							'.ts': 'never',
+							'.tsx': 'never',
+						}],
+
+						'@typescript-eslint/member-delimiter-style': 'error',
+
+						'@typescript-eslint/consistent-type-assertions': 'error',
+
+						// Allow functions to be passed in only in TS because it's easy to see their types
+						'unicorn/no-array-callback-reference': 'off',
+
+						// Not always possible to destructue at top-level when the variable is ambigious
+						'unicorn/consistent-destructuring': 'off',
+
+						// Could be used to pass in an explicit `undefined` to a required parameter
+						'unicorn/no-useless-undefined': 'off',
+
+						// https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-shadow.md
+						'no-shadow': 'off',
+						'@typescript-eslint/no-shadow': 'error',
+					},
+				},
+			],
+		}
+		: {
+			ignorePatterns: ['*.ts'],
+		}
+);
 
 module.exports = config;
