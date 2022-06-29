5d9c92d299dde8acd9dfa4b74137731a22ef2f7f
Hiroki Osame <hiroki.osame@gmail.com>
2022-06-29 12:54:20 -0400

    revert: debugging

--- tests/markdown/index.ts
+++ tests/markdown/index.ts
@@ -11,7 +11,6 @@ export default testSuite(({ describe }) => {
 			const results = await eslint.lintFiles([fixturePass]);
 			const { messages } = results[0];
 
-			console.log(messages);
 			expect(messages.length).toBe(2);
 			expect(messages).toEqual(
 				expect.arrayContaining([
