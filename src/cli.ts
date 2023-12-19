539576d0ed5d4ce284d0f658b39c032bd113c430
Hiroki Osame <hiroki.osame@gmail.com>
2023-12-20 00:23:49 +0900

    wip

--- src/cli.ts
+++ src/cli.ts
@@ -1,31 +1,56 @@
 import { cli } from 'cleye';
 import eslintApi from 'eslint/use-at-your-own-risk';
-import pvtnbr from '#pvtnbr';
+import { pvtnbr } from '#pvtnbr';
 
-console.log(pvtnbr);
+const argv = cli({
+	name: 'lint',
+	parameters: ['<files...>'],
+	help: {
+		description: 'by @pvtnbr/eslint-config',
+	},
+	flags: {
+		fix: {
+			type: Boolean,
+			description: 'Automatically fix problems',
+		},
+		quiet: {
+			type: Boolean,
+			description: 'Report errors only',
+		},
+		cache: {
+			type: Boolean,
+			description: 'Only check changed files',
+		},
+		cacheLocation: {
+			type: String,
+			description: 'Path to the cache file or directory',
+		},
+	},
+});
 
+(async () => {
+	const { FlatESLint } = eslintApi;
+	const eslint = new FlatESLint({
+		fix: argv.flags.fix,
+		baseConfig: pvtnbr(),
+		cache: argv.flags.cache,
+		cacheLocation: argv.flags.cacheLocation,
+	});
 
-// const argv = cli({
-// 	name: 'lint',
-// 	parameters: ['<files...>'],
-// 	help: {
-// 		description: 'by @pvtnbr/eslint-config',
-// 	},
-// 	flags: {
-// 		fix: Boolean,
-// 	},
-// });
+	const results = await eslint.lintFiles(argv._.files);
 
-// (async () => {
-// 	console.log(argv);
+	if (argv.flags.fix) {
+		await FlatESLint.outputFixes(results);
+	}
 
-// 	const eslint = new eslintApi.FlatESLint({
-	
-// 	});
-	
-// 	const result = await eslint.lintFiles(argv._.files);
-	
-// 	console.log(result);
-	
+	let resultsToPrint = results;
+	if (argv.flags.quiet) {
+		resultsToPrint = FlatESLint.getErrorResults(results);
+	}
 
-// })();
+	const formatter = await eslint.loadFormatter();
+	const output = await formatter.format(resultsToPrint);
+	if (output) {
+		console.log(output);
+	}
+})();
