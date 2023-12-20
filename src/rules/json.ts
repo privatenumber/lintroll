117d7e1f524915a809f5e34498f4d215fff4d517
Hiroki Osame <hiroki.osame@gmail.com>
2023-12-20 13:59:28 +0900

    feat(tsconfig.json): sort keys

--- src/rules/json.ts
+++ src/rules/json.ts
@@ -35,29 +35,42 @@ export const json = [
 				{
 					pathPattern: '^$',
 					order: [
+						// Publish requirements
 						'name',
 						'version',
-						'private',
-						'publishConfig',
+
+						// Package meta
 						'description',
 						'keywords',
 						'license',
 						'repository',
 						'funding',
 						'author',
-						'type',
+
+						// Privacy/publishing
+						'private',
+						'publishConfig',
 						'files',
+
+						// Package features
+						'type',
 						'bin',
+
+						// Entry points
 						'main',
 						'module',
 						'types',
 						'exports',
 						'imports',
 						'unpkg',
+
+						// Development meta
 						'scripts',
 						'husky',
 						'simple-git-hooks',
 						'lint-staged',
+
+						// Dependencies
 						'engines',
 						'peerDependencies',
 						'peerDependenciesMeta',
@@ -66,19 +79,145 @@ export const json = [
 						'devDependencies',
 						'bundledDependencies',
 						'bundleDependencies',
+
+						// Miscellaneous
 						'overrides',
 						'eslintConfig',
 					],
 				},
 				{
-					pathPattern: '^(dependencies|(dev|peer|optional|bundled)Dependencies)$',
+					pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies(Meta)?$',
 					order: { type: 'asc' },
 				},
 			],
 		},
 	}),
 	defineConfig({
-		files: ['**/tsconfig.json'],
-		rules: jsoncPlugin.configs['recommended-with-jsonc'].rules as Linter.RulesRecord,
+		files: ['**/tsconfig.json', '**/tsconfig.*.json'],
+		rules: {
+			...jsoncPlugin.configs['recommended-with-jsonc'].rules as Linter.RulesRecord,
+			'jsonc/sort-keys': [
+				'error',
+				{
+					pathPattern: '^$',
+					order: [
+						'extends',
+						'compilerOptions',
+						'references',
+						'files',
+						'include',
+						'exclude',
+					],
+				},
+				{
+					pathPattern: '^compilerOptions$',
+					order: [
+						// Projects
+						'incremental',
+						'composite',
+						'tsBuildInfoFile',
+						'disableSourceOfProjectReferenceRedirect',
+						'disableSolutionSearching',
+						'disableReferencedProjectLoad',
+
+						// Language and Environment
+						'target',
+						'jsx',
+						'jsxFactory',
+						'jsxFragmentFactory',
+						'jsxImportSource',
+						'lib',
+						'moduleDetection',
+						'noLib',
+						'reactNamespace',
+						'useDefineForClassFields',
+						'emitDecoratorMetadata',
+						'experimentalDecorators',
+
+						// Modules
+						'baseUrl',
+						'rootDir',
+						'rootDirs',
+						'customConditions',
+						'module',
+						'moduleResolution',
+						'moduleSuffixes',
+						'noResolve',
+						'paths',
+						'resolveJsonModule',
+						'resolvePackageJsonExports',
+						'resolvePackageJsonImports',
+						'typeRoots',
+						'types',
+						'allowArbitraryExtensions',
+						'allowImportingTsExtensions',
+						'allowUmdGlobalAccess',
+
+						// JavaScript Support
+						'allowJs',
+						'checkJs',
+						'maxNodeModuleJsDepth',
+
+						// Type Checking
+						'strict',
+						'strictBindCallApply',
+						'strictFunctionTypes',
+						'strictNullChecks',
+						'strictPropertyInitialization',
+						'allowUnreachableCode',
+						'allowUnusedLabels',
+						'alwaysStrict',
+						'exactOptionalPropertyTypes',
+						'noFallthroughCasesInSwitch',
+						'noImplicitAny',
+						'noImplicitOverride',
+						'noImplicitReturns',
+						'noImplicitThis',
+						'noPropertyAccessFromIndexSignature',
+						'noUncheckedIndexedAccess',
+						'noUnusedLocals',
+						'noUnusedParameters',
+						'useUnknownInCatchVariables',
+
+						// Emit
+						'declaration',
+						'declarationDir',
+						'declarationMap',
+						'downlevelIteration',
+						'emitBOM',
+						'emitDeclarationOnly',
+						'importHelpers',
+						'importsNotUsedAsValues',
+						'inlineSourceMap',
+						'inlineSources',
+						'mapRoot',
+						'newLine',
+						'noEmit',
+						'noEmitHelpers',
+						'noEmitOnError',
+						'outDir',
+						'outFile',
+						'preserveConstEnums',
+						'preserveValueImports',
+						'removeComments',
+						'sourceMap',
+						'sourceRoot',
+						'stripInternal',
+
+						// Interop Constraints
+						'allowSyntheticDefaultImports',
+						'esModuleInterop',
+						'forceConsistentCasingInFileNames',
+						'isolatedModules',
+						'preserveSymlinks',
+						'verbatimModuleSyntax',
+
+						// Completeness
+						'skipDefaultLibCheck',
+						'skipLibCheck',
+					],
+				},
+			],
+		},
 	}),
 ];
