f0281ed249eb09e1e6a4f04b27416b6e149894d6
Hiroki Osame <hiroki.osame@gmail.com>
2023-12-27 22:48:40 +0900

    wip

---
+++ tests/custom-rules/no-function-hoisting.ts
@@ -0,0 +1,84 @@
+import { testSuite } from 'manten';
+import { RuleTester } from '@typescript-eslint/rule-tester';
+import { noFunctionHoisting } from '../../src/custom-rules/no-function-hoisting/index.js';
+
+export default testSuite(({ describe }) => {
+	describe('no-function-hoisting', ({ describe, test }) => {
+		RuleTester.describe = describe;
+		RuleTester.it = test;
+		RuleTester.afterAll = () => {};
+		const ruleTester = new RuleTester({
+			parser: '@typescript-eslint/parser',
+		});
+
+		ruleTester.run('no-function-hoisting', noFunctionHoisting, {
+			valid: [
+				{
+					name: 'no hoisting',
+					code: 'function a(){}a()',
+				},
+			],
+
+			invalid: [
+				{
+					name: 'reverse hoisting',
+					code: 'a();function a(){}',
+					errors: [{
+						messageId: 'noFunctionHoisting',
+					}],
+					output: 'function a(){}a();',
+				},
+				{
+					name: 'reverse hoisting / preserve scope / top-level',
+					code: '(function(){a()});function a(){}',
+					errors: [{
+						messageId: 'noFunctionHoisting',
+					}],
+					output: 'function a(){}(function(){a()});',
+				},
+				{
+					name: 'reverse hoisting / preserve scope / nested',
+					code: '(function(){a();function a(){}})',
+					errors: [{
+						messageId: 'noFunctionHoisting',
+					}],
+					output: '(function(){function a(){}a();})',
+				},
+				{
+					name: 'reverse hoisting / preserve scope / nested',
+					code: 'while(a()){}function a(){}',
+					errors: [{
+						messageId: 'noFunctionHoisting',
+					}],
+					output: 'function a(){}while(a()){}',
+				},
+				{
+					name: 'reverse hoisting / preserve scope / nested',
+					code: '(a());function a(){}',
+					errors: [{
+						messageId: 'noFunctionHoisting',
+					}],
+					output: 'function a(){}(a());',
+				},
+				{
+					name: 'reverse hoisting / preserve scope / nested',
+					code: 'export{a}\nfunction a(){}',
+					errors: [{
+						messageId: 'noFunctionHoisting',
+					}],
+					output: 'function a(){}export{a}\n',
+				},
+				{
+					name: 'reverse hoisting / preserve scope / nested',
+					code: 'b();function a(){}function b(){a();}',
+					errors: [{
+						messageId: 'noFunctionHoisting',
+					}],
+					output: 'function a(){}function b(){a();}b();',
+				},
+
+				// Move comment before function too
+			],
+		});
+	});
+});
