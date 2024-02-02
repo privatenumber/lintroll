ba59e2b91508e2d57dd41c70c5c63d879e995958
Hiroki Osame <hiroki.osame@gmail.com>
2024-02-02 15:34:41 +0900

    fix(resolver): handle non-existent import map entry

--- src/resolvers/pkg-maps-resolver.ts
+++ src/resolvers/pkg-maps-resolver.ts
@@ -1,49 +1,95 @@
+import path from 'node:path';
 import fs from 'node:fs';
 import { findUpSync } from 'find-up-simple';
 import { resolveImports } from 'resolve-pkg-maps';
-import type { PackageJson } from 'read-package-up';
+import type { PackageJson } from 'type-fest';
 import { getConditions } from 'get-conditions';
 
-const notfound = Object.freeze({ found: false });
+const notFound = Object.freeze({ found: false });
 
-export const resolve = (
-	source: string,
-	file: string,
+const getResolvedPaths = (
+	object: PackageJson.Exports,
+): string[] => {
+	if (!object) {
+		return [];
+	}
+	if (typeof object === 'string') {
+		return [object];
+	}
+	return Object.values(object).flatMap(getResolvedPaths);
+};
+
+const tryFallback = (
+	imports: PackageJson.Imports,
+	source: `#${string}`,
+	excludePath?: string,
 ) => {
-	if (source[0] === '#') {
-		const packageJsonPath = findUpSync(
-			'package.json',
-			{ cwd: file },
-		);
+	const entryPaths = getResolvedPaths(imports[source])
+		.filter(resolvedPath => (
+			resolvedPath !== excludePath
+				&& !resolvedPath.endsWith('.d.ts')
+				&& fs.existsSync(resolvedPath)
+		));
 
-		if (!packageJsonPath) {
-			return notfound;
-		}
-
-		const pacakageJsonString = fs.readFileSync(packageJsonPath, 'utf8');
-		const packageJson = JSON.parse(pacakageJsonString) as PackageJson;
-
-		if (!packageJson.imports) {
-			return notfound;
-		}
-
-		const result = resolveImports(
-			packageJson.imports,
-			source,
-			getConditions(),
-		);
-
-		if (!result || result.length === 0) {
-			return notfound;
-		}
-
-		return {
-			found: true,
-			path: result[0],
-		};
+	if (entryPaths.length === 0) {
+		return notFound;
 	}
 
-	return notfound;
+	return {
+		found: true,
+		path: entryPaths[0],
+	};
+};
+
+export const resolve = (
+	source: `#${string}`,
+	file: string,
+) => {
+	if (source[0] !== '#') {
+		return notFound;
+	}
+
+	const packageJsonPath = findUpSync(
+		'package.json',
+		{ cwd: file },
+	);
+
+	if (!packageJsonPath) {
+		return notFound;
+	}
+
+	const pacakageJsonString = fs.readFileSync(packageJsonPath, 'utf8');
+	const { imports } = JSON.parse(pacakageJsonString) as PackageJson;
+
+	if (!imports) {
+		return notFound;
+	}
+
+	if (!(source in imports)) {
+		return notFound;
+	}
+
+	const result = resolveImports(
+		imports,
+		source,
+		getConditions(),
+	);
+
+	if (!result || result.length === 0) {
+		return tryFallback(imports, source);
+	}
+
+	const foundPath = result[0];
+	const foundPathResolved = path.resolve(packageJsonPath, '..', foundPath);
+
+	if (!fs.existsSync(foundPathResolved)) {
+		return tryFallback(imports, source, foundPath);
+	}
+
+	return {
+		found: true,
+		path: foundPath,
+	};
 };
 
 export const interfaceVersion = 2;
