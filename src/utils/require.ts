0ec2d7f4e89e603c39e74825cb605517c42c5859
Hiroki Osame <hiroki.osame@gmail.com>
2025-06-12 12:38:40 +0900

    fix: detect current package name

--- src/utils/require.ts
+++ src/utils/require.ts
@@ -1,7 +1,27 @@
 import { createRequire } from 'node:module';
+import { findUpSync } from 'find-up-simple';
 
 // Require from the context of the current project
 const require = createRequire(`${process.cwd()}/`);
+
+let currentPackageName: string | undefined;
+const getCurrentPackageName = () => {
+	if (currentPackageName) {
+		return currentPackageName;
+	}
+
+	const currentPackageJson = findUpSync('package.json');
+	if (!currentPackageJson) {
+		return;
+	}
+
+	try {
+		// eslint-disable-next-line import-x/no-dynamic-require
+		const packageJson = require(currentPackageJson);
+		currentPackageName = packageJson.name;
+		return currentPackageName;
+	} catch {}
+};
 
 export const isInstalled = (specifier: string) => {
 	try {
@@ -9,7 +29,7 @@ export const isInstalled = (specifier: string) => {
 		return true;
 	} catch {}
 
-	return false;
+	return getCurrentPackageName() === specifier;
 };
 
 export const getExports = (
